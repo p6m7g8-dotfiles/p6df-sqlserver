66dabdc7cd27c0728cebc122638cc30e74ef40d7
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-01-19 11:46:57 -0500

    fix

--- lib/cli.sh
+++ lib/cli.sh
@@ -11,7 +11,7 @@ p6df::modules::sqlserver::cli::run() {
 
   local now_eps=$(p6_dt_now_epoch_seconds)
 
-  docker run -d --name sqlserver-${now_eps} -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=$SA_PASSWORD' -p 1433:1433 microsoft/mssql-server-linux
+  docker run -d --name "sqlserver-${now_eps}" -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=$SA_PASSWORD' -p 1433:1433 microsoft/mssql-server-linux
 
   p6_return_void
 }
